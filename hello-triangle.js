d2e70e250cfbe902d968fa78e2a6c1c98d80edcd
Mustafa Ahmed <mustafa.ahmed1118@gmail.com>
2025-01-28 19:47:33 -0500

    implemented error checks

---
+++ hello-triangle.js
@@ -0,0 +1,9 @@
+function showError(errorText){
+    const errorBoxDiv = document.getElementById('error-box');
+    const errorTextElement = document.createElement('p');
+    errorTextElement.innerText = errorText;
+    errorBoxDiv.appendChild(errorTextElement);
+    console.log(errorText);
+}
+
+showError('This is  what an error looks like!');
